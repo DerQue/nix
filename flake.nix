0fa6c88ecfdc90e887bdb042ed8bf3f218396e2e
Quentin Schuster <me@quentin-schuster.de>
2026-04-17 13:55:15 +0200

    set homemanager with shell integration

--- flake.nix
+++ flake.nix
@@ -3,29 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+      # List packages installed in system profile.
       environment.systemPackages =
         [ 
 	  pkgs.neovim
 	  pkgs.git
+
 	  pkgs.alacritty
           pkgs.starship
+          pkgs.fish
         ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
       # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-      programs.zsh.enable = true;
+      programs.fish.enable = true;
+      #programs.zsh.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -36,13 +41,26 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+
+      fonts.packages = with pkgs; [
+    	nerd-fonts.fira-code # Neue Syntax für Nerd Fonts
+    	# Falls du eine ältere Nixpkgs-Version nutzt, stattdessen:
+    	# (nerdfonts.override { fonts = [ "FiraCode" ]; })
+      ];
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."macbookpro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+	configuration
+	home-manager.darwinModules.home-manager
+	{
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.quentin = ./home.nix;
+        }	 
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "quentin";
+  home.homeDirectory = "/Users/quentin";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.fish = {
+    enable = true;
+    
+    shellAliases = {
+      ll = "ls -la";
+      update = "darwin-rebuild switch --flake ~/.dotfiles#macbook"; # Passe den Pfad an
+    };
+
+    interactiveShellInit = ''
+      set -g fish_greeting ""
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true; 
+    
+    settings = {
+      add_newline = true;
+      # Hier kannst du deine komplette starship.toml Logik abbilden
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    
+    settings = {
+      window = {
+        padding = { x = 10; y = 10; };
+        decorations = "buttonless";
+        dynamic_padding = true;
+      };
+
+      font = {
+        normal = { family = "FiraCode Nerd Font"; style = "Regular"; };
+        size = 14.0;
+      };
+      
+      # Optional: macOS-Tastenkürzel explizit mappen (cmd+c, cmd+v, etc.)
+      keyboard.bindings = [
+        { key = "C"; mods = "Command"; action = "Copy"; }
+        { key = "V"; mods = "Command"; action = "Paste"; }
+      ];
+    };
+  };
+}
