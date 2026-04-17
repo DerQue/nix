abc69886f0d3e9706f7a2c31359dc678046eb152
Quentin Schuster <me@quentin-schuster.de>
2026-04-17 17:44:01 +0200

    add fonts

--- flake.nix
+++ flake.nix
@@ -63,6 +63,26 @@
 
       stylix.enable = true;
       stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
+  stylix.fonts = {
+monospace = {
+    package = pkgs.nerd-fonts.jetbrains-mono;
+    name = "JetBrainsMono Nerd Font";
+  };
+  sansSerif = {
+    package = pkgs.dejavu_fonts;
+    name = "DejaVu Sans";
+  };
+  serif = {
+    package = pkgs.dejavu_fonts;
+    name = "DejaVu Serif";
+  };
+
+    emoji = {
+      package = pkgs.noto-fonts-color-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
+
     };
   in
   {
